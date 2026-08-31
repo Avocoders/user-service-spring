37f2cdb33f6ae44c4c05a720dbf225ccbc27fd3a
Veronika Belova <belovaveronika14@gmail.com>
2026-08-31 16:03:47 +0300

    corrections and write in class User

--- src/main/java/io/github/avocoders/userservicespring/entity/User.java
+++ src/main/java/io/github/avocoders/userservicespring/entity/User.java
@@ -11,43 +11,52 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private Integer age;
-    @Column(name="created_at")
+    @Column(name="created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    public User () {}
+    public User() {}
 
-    public User (String name, String email, Integer age) {
+    public User(String name, String email, Integer age) {
         this.name = name;
         this.email = email;
         this.age = age;
     }
 
-    public void setName (String name) {
+    public void setName(String name) {
         this.name = name;
     }
-    public void setEmail (String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
-    public void setAge (Integer age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
-    public String getName () {
+    public String getName() {
         return this.name;
     }
-    public String getEmail () {
+    public String getEmail() {
         return this.email;
     }
-    public Integer getAge () {
+    public Integer getAge() {
         return this.age;
     }
-    public Long getId () {
+    public Long getId() {
         return this.id;
     }
-    public LocalDateTime getCreatedAt () {
+    public LocalDateTime getCreatedAt() {
         return this.createdAt;
     }
+
+    @PrePersist
+    private void initializeCreatedAt() {
+        createdAt = LocalDateTime.now();
+    }
+
 }
