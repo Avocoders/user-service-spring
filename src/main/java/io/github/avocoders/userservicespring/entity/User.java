f854f09d63c8b4cb7498546b627fbc242f79827b
Veronika Belova <belovaveronika14@gmail.com>
2026-08-31 15:16:19 +0300

    write in class User

--- src/main/java/io/github/avocoders/userservicespring/entity/User.java
+++ src/main/java/io/github/avocoders/userservicespring/entity/User.java
@@ -10,11 +10,44 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String name;
-    String email;
-    Integer age;
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
     @Column(name="created_at")
-    LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
+    public User () {}
+
+    public User (String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+    public void setEmail (String email) {
+        this.email = email;
+    }
+    public void setAge (Integer age) {
+        this.age = age;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+    public String getEmail () {
+        return this.email;
+    }
+    public Integer getAge () {
+        return this.age;
+    }
+    public Long getId () {
+        return this.id;
+    }
+    public LocalDateTime getCreatedAt () {
+        return this.createdAt;
+    }
 }
