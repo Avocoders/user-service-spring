0be556246df9ed0da1690d9473245fc449607e22
Veronika Belova <belovaveronika14@gmail.com>
2026-08-31 17:56:24 +0300

    add annotations in CreateUserRequest for valid

--- src/main/java/io/github/avocoders/userservicespring/dto/CreateUserRequest.java
+++ src/main/java/io/github/avocoders/userservicespring/dto/CreateUserRequest.java
@@ -1,8 +1,23 @@
 package io.github.avocoders.userservicespring.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class CreateUserRequest {
+    @NotBlank
+    @Size(max = 100)
     private String name;
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
+    @NotNull
+    @Min(0)
+    @Max(150)
     private Integer age;
 
     public CreateUserRequest() {}
