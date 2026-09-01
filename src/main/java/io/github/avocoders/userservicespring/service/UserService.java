28c1edac6a9b300d381fb4176af2f91b9ca97298
Veronika Belova <belovaveronika14@gmail.com>
2026-09-01 18:04:36 +0300

    add method getById in UserService

--- src/main/java/io/github/avocoders/userservicespring/service/UserService.java
+++ src/main/java/io/github/avocoders/userservicespring/service/UserService.java
@@ -3,9 +3,12 @@ package io.github.avocoders.userservicespring.service;
 import io.github.avocoders.userservicespring.dto.CreateUserRequest;
 import io.github.avocoders.userservicespring.dto.UserResponse;
 import io.github.avocoders.userservicespring.entity.User;
+import io.github.avocoders.userservicespring.exception.UserNotFoundException;
 import io.github.avocoders.userservicespring.mapper.UserMapper;
 import io.github.avocoders.userservicespring.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,4 +25,11 @@ public class UserService {
         User savedUser = userRepository.save(user);
         return userMapper.toResponse(savedUser);
     }
+
+    public UserResponse getById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        User foundUser = optionalUser.orElseThrow(() -> new UserNotFoundException(id));
+        return userMapper.toResponse(foundUser);
+    }
+
 }
