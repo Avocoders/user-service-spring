638a6b33203356aba92a46197cb913e103835969
Veronika Belova <belovaveronika14@gmail.com>
2026-09-13 18:35:15 +0300

    add documention in controller

--- src/main/java/io/github/avocoders/userservicespring/controller/UserController.java
+++ src/main/java/io/github/avocoders/userservicespring/controller/UserController.java
@@ -4,6 +4,8 @@ import io.github.avocoders.userservicespring.dto.CreateUserRequest;
 import io.github.avocoders.userservicespring.dto.UpdateUserRequest;
 import io.github.avocoders.userservicespring.dto.UserResponse;
 import io.github.avocoders.userservicespring.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,8 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Создать пользователя")
+    @ApiResponse(responseCode = "201", description = "Пользователь создан")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponse create(
@@ -28,17 +32,23 @@ public class UserController {
         return userService.create(request);
     }
 
+    @Operation(summary = "Получить пользователя по id")
+    @ApiResponse(responseCode = "200", description = "Пользователь найден")
     @GetMapping("/{id}")
     public UserResponse getById(
             @PathVariable Long id) {
         return userService.getById(id);
     }
 
+    @Operation(summary = "Получить список всех пользователей")
+    @ApiResponse(responseCode = "200", description = "Список пользователей получен")
     @GetMapping
     public List<UserResponse> getAll() {
         return userService.getAll();
     }
 
+    @Operation(summary = "Обновить пользователя по id")
+    @ApiResponse(responseCode = "200", description = "Пользователь обновлен")
     @PutMapping("/{id}")
     public UserResponse update(
             @PathVariable
@@ -49,6 +59,8 @@ public class UserController {
         return userService.update(id, request);
     }
 
+    @Operation(summary = "Удалить пользователя по id")
+    @ApiResponse(responseCode = "204", description = "Пользователь удален")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(
