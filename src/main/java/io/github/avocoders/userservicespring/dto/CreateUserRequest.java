959932b4180909248d57765da609c1aa35456ca7
Veronika Belova <belovaveronika14@gmail.com>
2026-09-13 19:05:59 +0300

    add documention in CreateUserRequest

--- src/main/java/io/github/avocoders/userservicespring/dto/CreateUserRequest.java
+++ src/main/java/io/github/avocoders/userservicespring/dto/CreateUserRequest.java
@@ -1,5 +1,6 @@
 package io.github.avocoders.userservicespring.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -7,14 +8,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Запрос на создание пользователя")
 public class CreateUserRequest {
+    @Schema(description = "Имя пользователя", example = "Nika")
     @NotBlank
     @Size(max = 100)
     private String name;
+    @Schema(description = "Почта пользователя", example = "nika@ya.ru")
     @NotBlank
     @Email
     @Size(max = 255)
     private String email;
+    @Schema(description = "Возраст пользователя", example = "20")
     @NotNull
     @Min(0)
     @Max(150)
